a8572cc6b39a6fa3351f500781ed90d54109fd25
yodasoda1219 <theo.github@bedafamily.com>
2021-08-05 16:28:32 -0700

    got a web server working!

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
-#include <iostream>
-#include <acme-lw.h>
-int main(int argc, const char** argv)
-{
-    acme_lw::AcmeClient::init();
-    acme_lw::AcmeClient::teardown();
+#include <drogon/drogon.h>
+void index_handler(const drogon::HttpRequestPtr& request_pointer, std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
+    auto response = drogon::HttpResponse::newHttpResponse();
+    response->addHeader("Content-Type", "text/html");
+    response->setBody("<h1>Hello, world!</h1>");
+    callback(response);
+}
+int main(int argc, const char** argv) {
+    auto& app = drogon::app();
+    app.registerHandler("/", std::function<void(const drogon::HttpRequestPtr&, std::function<void(const drogon::HttpResponsePtr&)>&&)>(index_handler), {drogon::Get});
+    app.addListener("127.0.0.1", 8080).run();
     return 0;
 }
